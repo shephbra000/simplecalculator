d2d6e509b551a8104a5f01c818761a67ef691ffa
shephbra000 <shephbra000@warren.k12.in.us>
2024-10-23 08:22:39 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+    grade = "A"
+
+    if grade >= 90 and grade <= 100:
+        grade = "A"
+    elif grade >= 80 and grade < 90:
+        grade = "B"
+    if grade >= 70 and grade <= 80
+        grade = "C"
+    elif grade >= 60 and grade < 70
+        grade = "D"
+else:
+    grade = "F"
+
+
+    print("Your grade is ", grade)
